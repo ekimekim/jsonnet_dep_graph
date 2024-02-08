decfe27efdcb40bd950bbbb1ef90f46c9d854d72
Mike Lang <mikelang3000@gmail.com>
2024-02-09 03:57:44 +1100

    Add library path support

--- README.md
+++ README.md
@@ -13,11 +13,31 @@ It will output one line per argument, in the form:
 ```
 FILE: DEP DEP DEP
 ```
+Note that FILE is included in the list of deps.
+
+#### Library paths
+
+This has basic support for library paths (`--jpath` on the `jsonnet` CLI), but it changes the behaviour
+somewhat as to disambiguate whether `import "foo.jsonnet"` refers to a relative file or a library path.
+
+Specify library paths by providing one or more `--jpath PATH` arguments.
+If at least one is given, then for each relative import, the following will be searched in order:
+- The directory of the file the import is in
+- Each library path, in the order given
+
+and uses the first path where that file currently exists.
+
+If any of the paths have an error besides "does not exist" (for example, a permission error),
+the whole process will fail.
+
+If the file does not exist anywhere, the directory of the file is assumed, but note that in most
+cases this will still lead to failure as we also need to read this file for further imports.
+
+The one case where a non-existent file won't cause problems is if it is imported via `importstr`
+and so doesn't need to be examined further. This may be useful in cases where such files are
+generated later.
 
 #### Limitations
-
-Does not support library paths as this makes import path resolution dependent on what files exist
-(eg. `import "foo.jsonnet"` will first try `./foo.jsonnet`, then `LIB/foo.jsonnet`).
 
 Uses jrsonnet 0.4.2 and would need a substantial rewrite to work with newer versions as the parser interface
 does not appear to be stable.
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,41 @@ use std::path::{Path, PathBuf};
 use std::collections::{HashMap, HashSet};
 use std::collections::hash_map::Entry;
 
+struct Resolver<'a> {
+	base_dir: &'a Path,
+	jpaths: &'a [&'a Path],
+}
+
+impl<'a> Resolver<'a> {
+	fn resolve(&self, path: &Path) -> Result<PathBuf, String> {
+		// If path is absolute, no need to check anything either as the prefix doesn't matter.
+		if path.is_absolute() {
+			return Ok(path.to_owned());
+		}
+		// If no jpaths set, this is a no-op and doesn't need to check for existence.
+		if self.jpaths.is_empty() {
+			return Ok(self.base_dir.join(path));
+		}
+		// Find the first extant match.
+		// Fail if we can't determine existence for any candidate.
+		for prefix in std::iter::once(self.base_dir).chain(self.jpaths.iter().copied()) {
+			let candidate = prefix.join(path);
+			let exists = candidate.try_exists().map_err(|e|
+				format!("Could not check path {}: {}", path.display(), e)
+			)?;
+			if exists {
+				return Ok(candidate);
+			}
+		}
+		// None existed, fall back to the local case.
+		// This seems more useful than erroring.
+		// It will likely error later anyway, when we try to parse that file.
+		// However, this behaviour is useful if the subject is a leaf dep
+		// that is a generated file.
+		return Ok(self.base_dir.join(path));
+	}
+}
+
 #[derive(Default, Debug)]
 struct Analysis {
 	// Leaf deps are static files, where only a change in the file itself
@@ -13,7 +48,7 @@ struct Analysis {
 	deep_deps: Vec<PathBuf>,
 }
 
-fn analyze_file(filepath: &Path) -> Result<Analysis, String> {
+fn analyze_file(jpaths: &[&Path], filepath: &Path) -> Result<Analysis, String> {
 	let contents = std::fs::read_to_string(filepath).map_err(|e|
 		format!("Failed to read {}: {}", filepath.display(), e)
 	)?;
@@ -27,118 +62,124 @@ fn analyze_file(filepath: &Path) -> Result<Analysis, String> {
 		format!("Failed to parse {}: {}", filepath.display(), e)
 	)?;
 
-	let mut analysis = Analysis::default();
 	// Path should always have a parent given we managed to open it as a file earlier, so it
 	// can't be a directory or "".
-	let base = filepath.parent().unwrap();
-	scan_ast(base, &mut analysis, &ast);
+	let base_dir = filepath.parent().unwrap();
+	let resolver = Resolver { base_dir, jpaths };
+
+	let mut analysis = Analysis::default();
+	scan_ast(&resolver, &mut analysis, &ast)?;
 
 	Ok(analysis)
 }
 
-fn add_path(base: &Path, paths: &mut Vec<PathBuf>, path: &Path) {
-	let path = base.join(path); // resolve to absolute path
+fn add_path(resolver: &Resolver, paths: &mut Vec<PathBuf>, path: &Path) -> Result<(), String> {
+	let path = resolver.resolve(path)?;
 	if !paths.contains(&path) {
 		paths.push(path);
 	}
+	Ok(())
 }
 
-fn scan_ast(base: &Path, analysis: &mut Analysis, expr: &LocExpr) {
+fn scan_ast(resolver: &Resolver, analysis: &mut Analysis, expr: &LocExpr) -> Result<(), String> {
 	match &*expr.0 {
 		// Base cases: We found actual imports!
-		Expr::Import(path) => add_path(base, &mut analysis.deep_deps, &path),
-		Expr::ImportStr(path) => add_path(base, &mut analysis.leaf_deps, &path),
+		Expr::Import(path) => add_path(resolver, &mut analysis.deep_deps, path)?,
+		Expr::ImportStr(path) => add_path(resolver, &mut analysis.leaf_deps, &path)?,
 		// Otherwise, recurse if needed
-		Expr::Arr(exprs) => for expr in exprs { scan_ast(base, analysis, expr) },
+		Expr::Arr(exprs) => for expr in exprs { scan_ast(resolver, analysis, expr)? },
 		Expr::ArrComp(expr, compspecs) => {
-			scan_ast(base, analysis, expr);
-			scan_compspecs(base, analysis, compspecs);
+			scan_ast(resolver, analysis, expr)?;
+			scan_compspecs(resolver, analysis, compspecs)?;
 		},
-		Expr::Obj(obj) => scan_obj(base, analysis, obj),
+		Expr::Obj(obj) => scan_obj(resolver, analysis, obj)?,
 		Expr::ObjExtend(expr, obj) => {
-			scan_ast(base, analysis, expr);
-			scan_obj(base, analysis, obj);
+			scan_ast(resolver, analysis, expr)?;
+			scan_obj(resolver, analysis, obj)?;
 		},
-		Expr::Parened(expr) => scan_ast(base, analysis, expr),
-		Expr::UnaryOp(_, expr) => scan_ast(base, analysis, expr),
+		Expr::Parened(expr) => scan_ast(resolver, analysis, expr)?,
+		Expr::UnaryOp(_, expr) => scan_ast(resolver, analysis, expr)?,
 		Expr::BinaryOp(expr_a, _, expr_b) => {
-			scan_ast(base, analysis, expr_a);
-			scan_ast(base, analysis, expr_b);
+			scan_ast(resolver, analysis, expr_a)?;
+			scan_ast(resolver, analysis, expr_b)?;
 		},
 		Expr::AssertExpr(AssertStmt(expr_a, maybe_expr_b), expr_c) => {
-			scan_ast(base, analysis, expr_a);
+			scan_ast(resolver, analysis, expr_a)?;
 			if let Some(expr) = maybe_expr_b {
-				scan_ast(base, analysis, expr);
+				scan_ast(resolver, analysis, expr)?;
 			}
-			scan_ast(base, analysis, expr_c);
+			scan_ast(resolver, analysis, expr_c)?;
 		},
 		Expr::LocalExpr(bindspecs, expr) => {
 			for bindspec in bindspecs {
-				scan_bindspec(base, analysis, bindspec);
+				scan_bindspec(resolver, analysis, bindspec)?;
 			}
-			scan_ast(base, analysis, expr);
+			scan_ast(resolver, analysis, expr)?;
 		},
-		Expr::ErrorStmt(expr) => scan_ast(base, analysis, expr),
+		Expr::ErrorStmt(expr) => scan_ast(resolver, analysis, expr)?,
 		Expr::Apply(expr, args, _) => {
-			scan_ast(base, analysis, expr);
+			scan_ast(resolver, analysis, expr)?;
 			for Arg(_, expr) in &args.0 {
-				scan_ast(base, analysis, expr);
+				scan_ast(resolver, analysis, expr)?;
 			}
 		},
 		Expr::Index(expr_a, expr_b) => {
-			scan_ast(base, analysis, expr_a);
-			scan_ast(base, analysis, expr_b);
+			scan_ast(resolver, analysis, expr_a)?;
+			scan_ast(resolver, analysis, expr_b)?;
 		},
 		Expr::Function(params, expr) => {
 			for Param(_, maybe_expr) in &*params.0 {
 				if let Some(expr) = maybe_expr {
-					scan_ast(base, analysis, expr);
+					scan_ast(resolver, analysis, expr)?;
 				}
 			}
-			scan_ast(base, analysis, expr);
+			scan_ast(resolver, analysis, expr)?;
 		},
 		Expr::IfElse{cond, cond_then, cond_else} => {
-			scan_ast(base, analysis, &cond.0);
-			scan_ast(base, analysis, cond_then);
+			scan_ast(resolver, analysis, &cond.0)?;
+			scan_ast(resolver, analysis, cond_then)?;
 			if let Some(expr) = cond_else {
-				scan_ast(base, analysis, expr);
+				scan_ast(resolver, analysis, expr)?;
 			}
 		},
 		Expr::Slice(expr, SliceDesc{start, end, step}) => {
-			scan_ast(base, analysis, expr);
+			scan_ast(resolver, analysis, expr)?;
 			for maybe_expr in [start, end, step] {
 				if let Some(expr) = maybe_expr {
-					scan_ast(base, analysis, expr);
+					scan_ast(resolver, analysis, expr)?;
 				}
 			}
 		},
 		// Remaining cases are leaf nodes like literals that we don't care about.
 		_ => (),
 	}
+	Ok(())
 }
 
-fn scan_compspecs(base: &Path, analysis: &mut Analysis, compspecs: &[CompSpec]) {
+fn scan_compspecs(resolver: &Resolver, analysis: &mut Analysis, compspecs: &[CompSpec]) -> Result<(), String> {
 	for compspec in compspecs {
 		match compspec {
-			CompSpec::IfSpec(data) => scan_ast(base, analysis, &data.0),
-			CompSpec::ForSpec(data) => scan_ast(base, analysis, &data.1),
+			CompSpec::IfSpec(data) => scan_ast(resolver, analysis, &data.0)?,
+			CompSpec::ForSpec(data) => scan_ast(resolver, analysis, &data.1)?,
 		}
 	}
+	Ok(())
 }
 
-fn scan_bindspec(base: &Path, analysis: &mut Analysis, bindspec: &BindSpec) {
+fn scan_bindspec(resolver: &Resolver, analysis: &mut Analysis, bindspec: &BindSpec) -> Result<(), String> {
 	let BindSpec{params, value, ..} = bindspec;
 	if let Some(params) = params {
 		for Param(_, maybe_expr) in &*params.0 {
 			if let Some(expr) = maybe_expr {
-				scan_ast(base, analysis, expr);
+				scan_ast(resolver, analysis, expr)?;
 			}
 		}
 	}
-	scan_ast(base, analysis, value);
+	scan_ast(resolver, analysis, value)?;
+	Ok(())
 }
 
-fn scan_obj(base: &Path, analysis: &mut Analysis, obj: &ObjBody) {
+fn scan_obj(resolver: &Resolver, analysis: &mut Analysis, obj: &ObjBody) -> Result<(), String> {
 	match obj {
 		ObjBody::MemberList(members) => {
 			for member in members {
@@ -146,38 +187,39 @@ fn scan_obj(base: &Path, analysis: &mut Analysis, obj: &ObjBody) {
 					Member::Field(FieldMember{name, params, value, ..}) => {
 						match name {
 							FieldName::Fixed(_) => (),
-							FieldName::Dyn(expr) => scan_ast(base, analysis, expr),
+							FieldName::Dyn(expr) => scan_ast(resolver, analysis, expr)?,
 						}
 						if let Some(params) = params {
 							for Param(_, maybe_expr) in &*params.0 {
 								if let Some(expr) = maybe_expr {
-									scan_ast(base, analysis, expr);
+									scan_ast(resolver, analysis, expr)?;
 								}
 							}
 						}
-						scan_ast(base, analysis, value);
+						scan_ast(resolver, analysis, value)?;
 					},
-					Member::BindStmt(bindspec) => scan_bindspec(base, analysis, bindspec),
+					Member::BindStmt(bindspec) => scan_bindspec(resolver, analysis, bindspec)?,
 					Member::AssertStmt(AssertStmt(expr, maybe_expr)) => {
-						scan_ast(base, analysis, expr);
+						scan_ast(resolver, analysis, expr)?;
 						if let Some(expr) = maybe_expr {
-							scan_ast(base, analysis, expr);
+							scan_ast(resolver, analysis, expr)?;
 						}
 					},
 				}
 			}
 		},
 		ObjBody::ObjComp(ObjComp{pre_locals, key, value, post_locals, compspecs}) => {
-			for bindspec in pre_locals { scan_bindspec(base, analysis, bindspec); }
-			scan_ast(base, analysis, key);
-			scan_ast(base, analysis, value);
-			for bindspec in post_locals { scan_bindspec(base, analysis, bindspec); }
-			scan_compspecs(base, analysis, compspecs);
+			for bindspec in pre_locals { scan_bindspec(resolver, analysis, bindspec)?; }
+			scan_ast(resolver, analysis, key)?;
+			scan_ast(resolver, analysis, value)?;
+			for bindspec in post_locals { scan_bindspec(resolver, analysis, bindspec)?; }
+			scan_compspecs(resolver, analysis, compspecs)?;
 		},
 	}
+	Ok(())
 }
 
-fn resolve_deps(cache: &mut HashMap<PathBuf, Analysis>, filename: &Path) -> Result<HashSet<PathBuf>, String> {
+fn resolve_deps(cache: &mut HashMap<PathBuf, Analysis>, jpaths: &[&Path], filename: &Path) -> Result<HashSet<PathBuf>, String> {
 	let mut deps: HashSet<PathBuf> = HashSet::new();
 	let mut to_expand = vec![filename.to_owned()];
 	while let Some(filename) = to_expand.pop() {
@@ -192,7 +234,7 @@ fn resolve_deps(cache: &mut HashMap<PathBuf, Analysis>, filename: &Path) -> Resu
 		let analysis = match cache.entry(filename) {
 			Entry::Occupied(entry) => entry.into_mut(),
 			Entry::Vacant(entry) => {
-				let analysis = analyze_file(entry.key())?;
+				let analysis = analyze_file(jpaths, entry.key())?;
 				entry.insert(analysis)
 			}
 		};
@@ -226,8 +268,9 @@ fn inner_main() -> Result<(), String> {
 	}
 
 	let mut cache: HashMap<PathBuf, Analysis> = HashMap::new();
+	let jpaths: Vec<&Path> = jpaths.iter().map(|path| path.as_path()).collect();
 	for filepath in files {
-		let deps = resolve_deps(&mut cache, &filepath)?;
+		let deps = resolve_deps(&mut cache, &jpaths, &filepath)?;
 		let as_str: Vec<_> = deps.iter().map(|p| p.to_string_lossy()).collect();
 		println!("{}: {}", filepath.display(), as_str.join(" "));
 	}
